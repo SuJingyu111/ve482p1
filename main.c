1be068d360ec1f1f5aef17ee03ab51a7e817857b
yaxinchen666 <51110939+yaxinchen666@users.noreply.github.com>
2019-09-18 11:13:18 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MAXCHAC 1024 //maximum #characters in a line
 #define MAXCINW 256 //maximum #characters in a word
-#define MAXPIPELINE 32 // maximum #pipeline
+#define MAXPIPELINE 16 // maximum #pipeline
+#define MAXREDIRECTION 128 // maximum #redirection
 
-char ** parse(const char *line, char ** arg, int *numarg, int *numcm, int *cmpos)
+enum{ROUT, ROUTA, RIN}; // >, >>, <
+
+struct redirectsymbol{
+    int pos; // after which argument
+    int symbol; // >, >>, <
+};
+
+char ** parse(const char *line, char ** arg, int *numarg, int *numrd, struct  redirectsymbol *rd, int *numcm, int *cmpos)
 // REQUIRES: *numcm = *numarg = 0
 // EFFECTS: separate line into arg[] according to white space;
 //          numcm records the number of command;
@@ -55,16 +65,45 @@ char ** parse(const char *line, char ** arg, int *numarg, int *numcm, int *cmpos
             i++; //skip the last "
         }*/
 
-        while(line[i] != ' ' && line[i] != '\n'){
+        while(line[i] != ' ' && line[i] != '>' && line[i] != '<' && line[i] != '\n'){
             arg[*numarg][j]=line[i];
             j++;
             i++;
         }
         arg[*numarg][j] = '\0';
 
+        while(line[i] == ' ') i++;
+
+        if(line[i] == '>'){
+            if(i<(MAXCHAC+2) && line[i+1]=='>'){
+                struct redirectsymbol temp;
+                temp.pos=(*numarg);
+                temp.symbol=ROUTA;
+                rd[*numrd] = temp;
+                (*numrd)++;
+                i++;
+            }
+            else{
+                struct redirectsymbol temp;
+                temp.pos=(*numarg);
+                temp.symbol=ROUT;
+                rd[*numrd] = temp;
+                (*numrd)++;
+            }
+            i++;
+        }
+        else if(line[i] == '<'){
+            struct redirectsymbol temp;
+            temp.pos=(*numarg);
+            temp.symbol=RIN;
+            rd[*numrd] = temp;
+            (*numrd)++;
+            i++;
+        }
+
         (*numarg)++;
         arg = (char **) realloc(arg, ((*numarg)+1) * sizeof(char *));
-        arg[(*numarg)] = (char *)malloc(sizeof(char) * MAXCINW);
+        arg[(*numarg)] = (char *)calloc(MAXCINW, sizeof(char));
 
     }
 
@@ -73,19 +112,118 @@ char ** parse(const char *line, char ** arg, int *numarg, int *numcm, int *cmpos
 }
 
 
-int runsimplecommand(char **arg)
+/*int runsimplecommand(char **arg)
 {
     pid_t child;
     int status;
     child = fork();
+    if(child == -1) {
+        printf("Failed to fork.\n");
+        fflush(stdout);
+        return 1;
+    }
     if(child != 0){
         wait(&status);
     }
     else{
-        execvp(arg[0], arg);
+        if(execvp(arg[0], arg)<0){
+            printf("Cannot execute \"%s\"!\n", arg[0]);
+            fflush(stdout);
+            exit(0);
+        }
+    }
+    return 0;
+}*/
+
+int runinoutcommand(char **arg, int numrd, struct redirectsymbol *rd)
+{
+    pid_t child;
+    int status;
+    child = fork();
+    if(child == -1) {
+        printf("Failed to fork.\n");
+        fflush(stdout);
+        return 1;
+    }
+    if(child != 0){
+        wait(&status);
+    }
+    else{
+        if(numrd == 0){
+            if(execvp(arg[0], arg)<0){
+                printf("Cannot execute \"%s\"!\n", arg[0]);
+                fflush(stdout);
+                exit(0);
+            }
+        }
+        else{
+            for(int i = 0; i < numrd-1; i++){
+                if(rd[i].pos >= rd[i+1].pos-1 && arg[rd[i+1].pos][0] == '\0'){
+                    printf("Lack file to redirect.\n");
+                    fflush(stdout);
+                    return 1;
+                }
+                int fp[rd[i+1].pos-rd[i].pos];
+                if(rd[i].symbol == ROUT){
+                    int numfp=0;
+                    for(int pos = rd[i].pos+1; pos < rd[i+1].pos; pos++){
+                        fp[numfp] = open(arg[pos], O_CREAT|O_TRUNC);
+                        if(fp[numfp] < 0){
+                            printf("Cannot open file %s", arg[pos]);
+                            fflush(stdout);
+                            return 1;
+                        }
+                        int fd = dup2(fp[numfp], STDOUT_FILENO);
+                        if(fd<0){
+                            printf("Cannot duplicat file descriptor.\n");
+                            fflush(stdout);
+                        }
+                        numfp++;
+                    }
+                }
+                else if(rd[i].symbol == ROUTA){
+                    int numfp=0;
+                    for(int pos = rd[i].pos+1; pos < rd[i+1].pos; pos++){
+                        fp[numfp] = open(arg[pos], O_CREAT|O_APPEND);
+                        if(fp[numfp] < 0){
+                            printf("Cannot open file %s", arg[pos]);
+                            fflush(stdout);
+                            return 1;
+                        }
+                        int fd = dup2(fp[numfp], STDOUT_FILENO);
+                        if(fd<0){
+                            printf("Cannot duplicat file descriptor.\n");
+                            fflush(stdout);
+                        }
+                        numfp++;
+                    }
+                }
+                else{
+                    int numfp=0;
+                    for(int pos = rd[i].pos+1; pos < rd[i+1].pos; pos++){
+                        //fp[numfp] = open(arg[pos], O_CREAT|O_APPEND);改
+                        if(fp[numfp] < 0){
+                            printf("Cannot open file %s", arg[pos]);
+                            fflush(stdout);
+                            return 1;
+                        }
+                        //int fd = dup2(fp[numfp], STDOUT_FILENO);改
+                        if(fd<0){
+                            printf("Cannot duplicat file descriptor.\n");
+                            fflush(stdout);
+                        }
+                        numfp++;
+                    }
+                }
+            }
+            //最后一个rd处理
+        }
     }
     return 0;
 }
+
+
+
 
 
 int main() {
@@ -95,9 +233,11 @@ int main() {
     while(1){
 
         printf("mumsh $ ");
+        fflush(stdout);
 
         if(fgets(line, MAXCHAC+1, stdin) == NULL){
             printf("Error in getting command.\n");
+            fflush(stdout);
             break;
         }
 
@@ -109,17 +249,19 @@ int main() {
         int numarg = 0, numcm = 0;
         char **arg;
         arg = (char **)malloc(sizeof(char *)); //first assign one argument
-        arg[0] = (char *)malloc(sizeof(char) * MAXCINW); //suppose #characters of a word will not exceed MAXCINW
+        arg[0] = (char *)calloc(MAXCINW, sizeof(char)); //suppose #characters of a word will not exceed MAXCINW
         int cmpos[MAXPIPELINE]; //suppose there are at most MAXPIPELINE pipelines
+        int numrd = 0;
+        struct redirectsymbol rd[MAXREDIRECTION];
 
-        arg = parse(line, arg, &numarg, &numcm, cmpos);
+        arg = parse(line, arg, &numarg, &numrd, rd, &numcm, cmpos);
         free(arg[numarg]);
         arg[numarg] = NULL;
 
 
 
 
-        runsimplecommand(arg);
+        //runsimplecommand(arg);
 
 
 
